33fcc2bf7c38803d8088ee6ea3a8e08b55d02e84
Okeomasilachi <okeomasilachi@gmail.com>
2023-10-31 18:15:43 +0100

    Write a function that measures the height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -32,5 +32,8 @@ size_t tree_height(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
+
 	return (tree_height(tree) - 1);
 }
