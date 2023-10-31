65033bcb67112cd2f39e9a26f37d7b15a63a2512
Okeomasilachi <okeomasilachi@gmail.com>
2023-10-31 16:06:43 +0100

    in-order and post-order traversal

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,1 +1,21 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - The function performs a inorder traversal
+ *		on a binary tree and applies a given function to each node.
+ *
+ * @tree: A pointer to the root of the binary tree.
+ * @func: The parameter `func` is a function pointer that points to
+ *		a function with the following
+ *
+ * Return: Void
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		binary_tree_inorder(tree->left, func);
+		func(tree->n);
+		binary_tree_inorder(tree->right, func);
+	}
+}
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,1 +1,23 @@
 #include "binary_trees.h"
+
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - The function performs a postorder traversal
+ *		on a binary tree and applies a given function to each node.
+ *
+ * @tree: A pointer to the root of the binary tree.
+ * @func: The parameter `func` is a function pointer that points to
+ *		a function with the following
+ *
+ * Return: Void
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+	}
+}
