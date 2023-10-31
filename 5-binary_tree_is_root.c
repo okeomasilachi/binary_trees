f5bf30c01aa389e51db65bd34b982616d5e587ad
Okeomasilachi <okeomasilachi@gmail.com>
2023-10-31 14:15:27 +0100

    Write a function that checks if a node is a leaf

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,1 +1,17 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - The function checks if a given node
+ *		in a binary tree is the root node.
+ * @node: A pointer to a node.
+ *
+ * Return: 1 if the given node is the root node
+ *		(i.e., it has no parent), and 0 otherwise.
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node && node->parent == NULL)
+		return (1);
+
+	return (0);
+}
