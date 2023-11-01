bf978b006b72a98e07126827370db3631884da1c
Okeomasilachi <okeomasilachi@gmail.com>
2023-11-01 13:22:56 +0100

    debugging for adding left childe

--- main.c
+++ main.c
@@ -8,21 +8,14 @@
 int main(void)
 {
     binary_tree_t *root;
-    int full;
 
     root = binary_tree_node(NULL, 98);
     root->left = binary_tree_node(root, 12);
     root->right = binary_tree_node(root, 402);
-    binary_tree_insert_right(root->left, 54);
-    binary_tree_insert_right(root, 128);
-    root->left->left = binary_tree_node(root->left, 10);
     binary_tree_print(root);
-
-    full = binary_tree_is_full(root);
-    printf("Is %d full: %d\n", root->n, full);
-    full = binary_tree_is_full(root->left);
-    printf("Is %d full: %d\n", root->left->n, full);
-    full = binary_tree_is_full(root->right);
-    printf("Is %d full: %d\n", root->right->n, full);
+    printf("\n");
+    binary_tree_insert_left(root->right, 128);
+    binary_tree_insert_left(root, 54);
+    binary_tree_print(root);
     return (0);
 }
