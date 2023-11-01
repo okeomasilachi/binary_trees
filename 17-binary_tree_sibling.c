8ebd7be105679d09943eeeae4b2082691f70b18b
Okeomasilachi <okeomasilachi@gmail.com>
2023-11-01 18:51:29 +0100

    Write a function that finds the sibling of a node

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,1 +1,24 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - returns the sibling of a given node in a
+ *		binary tree.
+ * @node: A pointer to a node in a binary tree.
+ *
+ * Return: a pointer to the sibling node of the given node
+ *		in a binary tree.
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *tmp = NULL;
+
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->right == node)
+		tmp = node->parent->left;
+	else if (node->parent->left == node)
+		tmp = node->parent->right;
+
+	return (tmp);
+}
--- main.c
+++ main.c
@@ -8,28 +8,26 @@
 int main(void)
 {
     binary_tree_t *root;
-    int perfect;
+    binary_tree_t *sibling;
 
     root = binary_tree_node(NULL, 98);
     root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
-    binary_tree_insert_right(root->left, 54);
-    binary_tree_insert_right(root, 128);
+    root->right = binary_tree_node(root, 128);
+    root->left->right = binary_tree_node(root->left, 54);
+    root->right->right = binary_tree_node(root->right, 402);
     root->left->left = binary_tree_node(root->left, 10);
-    root->right->left = binary_tree_node(root->right, 10);
+    root->right->left = binary_tree_node(root->right, 110);
+    root->right->right->left = binary_tree_node(root->right->right, 200);
+    root->right->right->right = binary_tree_node(root->right->right, 512);
 
     binary_tree_print(root);
-    perfect = binary_tree_is_perfect(root);
-    printf("Perfect: %d\n\n", perfect);
-
-    root->right->right->left = binary_tree_node(root->right->right, 10);
-    binary_tree_print(root);
-    perfect = binary_tree_is_perfect(root);
-    printf("Perfect: %d\n\n", perfect);
-
-    root->right->right->right = binary_tree_node(root->right->right, 10);
-    binary_tree_print(root);
-    perfect = binary_tree_is_perfect(root);
-    printf("Perfect: %d\n", perfect);
+    sibling = binary_tree_sibling(root->left);
+    printf("Sibling of %d: %d\n", root->left->n, sibling->n);
+    sibling = binary_tree_sibling(root->right->left);
+    printf("Sibling of %d: %d\n", root->right->left->n, sibling->n);
+    sibling = binary_tree_sibling(root->left->right);
+    printf("Sibling of %d: %d\n", root->left->right->n, sibling->n);
+    sibling = binary_tree_sibling(root);
+    printf("Sibling of %d: %p\n", root->n, (void *)sibling);
     return (0);
 }
