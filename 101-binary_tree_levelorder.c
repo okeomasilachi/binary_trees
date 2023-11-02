1219adcc84d7c41dff22d0085561784ca2af0b80
Okeomasilachi <okeomasilachi@gmail.com>
2023-11-02 12:32:56 +0100

    operation

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,1 +1,15 @@
 #include "binary_trees.h"
+
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		func(tree->n);
+		if (tree->left->n)
+			func(tree->left->n);
+		if (tree->right->n)
+			func(tree->right->n);
+		binary_tree_levelorder(tree->left, func);
+		binary_tree_levelorder(tree->right, func);
+	}
+}
